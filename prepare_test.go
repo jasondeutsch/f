822c60092fb935e71322ad3b23ef12d7d547d9bf
jasondeutsch <jndeutsch@gmail.com>
2021-09-19 15:29:05 -0500

    add tet for prepare()

--- prepare_test.go
+++ prepare_test.go
@@ -1,1 +1,58 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestPrepare(t *testing.T) {
+	tt := []struct {
+		contents string
+		expectName string
+		expectArgs []string
+	}{
+		{
+			contents: "ls",
+			expectName: "ls",
+			expectArgs: nil,
+		},
+		{
+			contents: " ls ",
+			expectName: "ls",
+			expectArgs: nil,
+		},
+		{
+			contents: " ls -la",
+			expectName: "ls",
+			expectArgs: []string{"-la"},
+		},
+		{
+			contents: `
+ls \
+-la
+`,
+            expectName: "ls",
+			expectArgs: []string{"-la"},
+		},
+	}
+
+	for _, tc := range tt {
+		name, args := prepare(tc.contents)
+		if name != tc.expectName || !stringSliceEqual(args, tc.expectArgs) {
+			t.Errorf("\ngot:\n%s, %v \nexpected:\n%s, %v", name, args, tc.expectName, tc.expectArgs)
+		}
+	}
+}
+
+func stringSliceEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
